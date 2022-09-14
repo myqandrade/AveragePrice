63c47ff69a91d068830975e8e9d8c0f894f83358
Mike Andrade <mike.andrade.oliveira@gmail.com>
2022-09-14 19:34:14 -0300

    Criando programa para calcular média de preço

---
+++ src/application/Program.java
@@ -0,0 +1,37 @@
+package application;
+
+import entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args){
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter product quantity: ");
+        int quantity = sc.nextInt();
+
+        Product[] products = new Product[quantity];
+        double sum = 0.0;
+
+        System.out.print("Enter product name: ");
+        String name = sc.next();
+
+        for(int i = 0; i < quantity; i++){
+            System.out.print("Enter product price: ");
+            double price = sc.nextDouble();
+            Product product = new Product(name, price);
+            products[i] = product;
+            sum += product.getPrice();
+        }
+
+        double averagePrice = sum / quantity;
+
+        System.out.println();
+        System.out.printf("AVERAGE PRICE: %.2f%n", averagePrice);
+    }
+}
---
+++ src/entities/Product.java
@@ -0,0 +1,17 @@
+package entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+
+    private String name;
+    private double price;
+
+    public Product(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+}
